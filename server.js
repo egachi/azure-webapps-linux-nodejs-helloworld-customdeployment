183bfdb8547abe579c7e8cf2b073888c1c5c267d
Edison Garcia Chinas <edisga@microsoft.com>
2018-07-16 14:00:30 -0500

    updating

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express()
 var port = process.env.port || 3000
 app.get('/', function (req, res) {
     res.send(process.version)
-  })
+  });
 app.listen(port, () => console.log('Example app listening on port 3000!'))
