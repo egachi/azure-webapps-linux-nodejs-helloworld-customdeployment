696c6d42a023fe939be4e9ec7b92b2543c826aa6
Edison Garcia Chinas <edisga@microsoft.com>
2018-08-09 18:10:06 -0500

    Updating

--- deploy.sh
+++ deploy.sh
@@ -63,7 +63,6 @@ if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
     KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
   fi
 fi
-
 ##################################################################################################################################
 # Deployment
 # ----------
