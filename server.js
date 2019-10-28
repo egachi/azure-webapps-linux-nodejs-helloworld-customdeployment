beb5d6df4bc7e8d257987544770a43040606515f
Edison Garcia <egachi@live.com>
2019-10-28 14:12:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var port = process.env.port || 3000
+var port = process.env.PORT || 3000
 app.get('/', function (req, res) {
     res.send(process.version)
   });
